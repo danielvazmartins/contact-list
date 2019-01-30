4b6877a8021bfb57e40b63350743d90b4b6bd0e9
Daniel Vaz Villalobos Martins <danielvazmartins@gmail.com>
2019-01-30 10:14:02 -0200

    salvando alterações

--- front/src/app/contact.service.ts
+++ front/src/app/contact.service.ts
@@ -8,8 +8,8 @@ import { Contact } from './contact.class';
 })
 export class ContactService {
 
-  //private hostApi = 'http://localhost:3000'
-  private hostApi = '/api'
+  private hostApi = 'http://localhost:3000/api'
+  //private hostApi = '/api'
   
   constructor( private http:HttpClient ) { 
   }
